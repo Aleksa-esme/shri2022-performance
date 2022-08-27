71e4f2fb956573b025f1f3d6b2edfe099e10016f
Aleksa-esme <esme1990@mail.ru>
2022-08-27 16:29:46 +0300

    aaa

---
+++ makeMenu.js
@@ -0,0 +1,14 @@
+Array.from(document.querySelectorAll('.header__menu')).forEach(makeMenu);
+
+function makeMenu(node) {
+    let expanded = false;
+    const links = document.querySelector('.header__links');
+
+    node.addEventListener('click', () => {
+        expanded = !expanded;
+        node.setAttribute('aria-expanded', expanded ? 'true' : 'false');
+        node.querySelector('.header__menu-text').textContent = expanded ? 'Закрыть меню' : 'Открыть меню';
+        links.classList.toggle('header__links_opened', expanded);
+        links.classList.add('header__links-toggled');
+    });
+}
